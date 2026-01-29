a217c42413989ea9abd174606a54c96081e18448
officialblacklover29-blip <officialblacklover29@gmail.com>
2026-01-29 23:00:04 +0530

    mm

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './Index.css'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './context/UserContext.jsx'; // 
 
